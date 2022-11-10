bbe321ed13d8072278ef32cafb4309e6f6937c67
George Tipton <geo@MB-RCG9G1VJJP.ghostlab>
2022-11-09 20:58:58 -0700

    finished functions

--- 14 functions/functions.go
+++ 14 functions/functions.go
@@ -215,3 +215,197 @@
 // 	fmt.Printf("%T\n", y)
 
 // }
+
+// Anonymous Func
+// package main
+
+// import "fmt"
+
+// func main() {
+// 	fmt.Println("Ayooo")
+// 	foo() //not anonymous
+
+// 	func(x int) { //anonymous
+// 		fmt.Println("Hello from anonymous func", x)
+// 	}(42)
+
+// }
+
+// func foo() { // not anonymous
+// 	fmt.Println("Hello from foo")
+// }
+
+// Func Expression
+// Assign a function to a variable <-- This
+// Funcs are first class citizens (can be used like any other type or variable)
+// package main
+
+// import "fmt"
+
+// func main() {
+// 	fmt.Println("Ayooo")
+// 	f := func() {
+// 		fmt.Println("my first func expression")
+// 	}
+// 	f()
+// 	g := func(x int) {
+// 		fmt.Println("The year big brother started watching: ", x)
+// 	}
+// 	g(1984)
+
+// }
+
+// Returning a Func
+// Returning a function from a function
+// package main
+
+// import "fmt"
+
+// func main() {
+// 	x := bar()
+// 	fmt.Printf("%T\n", bar())
+// 	fmt.Println(x())
+// 	fmt.Println(bar()()) //simplified
+// 	// x := func() int { //anonymous
+// 	// 	return 451
+// 	// }
+// 	//fmt.Printf("%T", x)
+
+// }
+
+// func foo() string {
+// 	s := "Hello from foo" // unnecessary (just return the string)
+// 	return s              // returning a string
+// }
+
+// func bar() func() int { // bar returns a function which returns an int
+// 	return func() int {
+// 		return 451
+// 	}
+// }
+
+// Callback
+// Passing a func as an argument
+// Sometimes known as functional programming
+// package main
+
+// import "fmt"
+
+// func main() {
+// 	ii := []int{1, 2, 3, 4}
+// 	s := sum(ii...) // must unfurl the slice of int
+// 	fmt.Println("all numbers:", s)
+
+// 	s2 := even(sum, ii...)
+// 	fmt.Println("even numbers:", s2)
+
+// 	s3 := odd(sum, ii...)
+// 	fmt.Println("odd numbers:", s3)
+// }
+
+// func sum(xi ...int) int {
+// 	fmt.Printf("%T\n", xi)
+// 	total := 0
+// 	for _, v := range xi {
+// 		total += v
+// 	}
+// 	return total
+// }
+
+// func even(f func(xi ...int) int, vi ...int) int { // Callback
+// 	var yi []int
+// 	for _, v := range vi {
+// 		if v%2 == 0 {
+// 			yi = append(yi, v)
+// 		}
+// 	}
+// 	return f(yi...)
+
+// }
+
+// func odd(f func(xi ...int) int, vi ...int) int { // Callback
+// 	var yi []int
+// 	for _, v := range vi {
+// 		if v%2 != 0 {
+// 			yi = append(yi, v)
+// 		}
+// 	}
+// 	return f(yi...)
+
+// }
+
+// Closure
+// Enclose the scope of a variable and contain it to a certain area
+// Enclose some code around the variable to limit the scope of the variable
+// package main
+
+// import "fmt"
+
+// // var x int // declare x, wider scope
+
+// func main() {
+// 	var x int      // narrower scope to func main()
+// 	fmt.Println(x) // 0
+// 	x++            // increment x here
+// 	{              // limiting the scope of y here
+// 		y := 1
+// 		fmt.Println(y) // note can print y here since its in scope
+// 	}
+// 	// fmt.Println(y) // ^^ but cant print y here because out of scope
+// 	fmt.Println(x) // 1
+// 	foo()          // note the scope of x (it is also in foo)
+// 	fmt.Println(x) // 2
+
+// 	a := incrementor()
+// 	b := incrementor()
+// 	fmt.Println(a())
+// 	fmt.Println(a())
+// 	fmt.Println(b())
+// 	fmt.Println(b())
+
+// }
+
+// func foo() {
+// 	// x++ // increment x here
+// 	fmt.Println("foo")
+// }
+
+// func incrementor() func() int { // incrementor example with closure
+// 	var x int
+// 	return func() int {
+// 		x++
+// 		return x
+// 	}
+// }
+
+// Recursion
+// Anything you do with recursion, you can do with loops
+// Recursion is often times overly complex and can have memory drawbacks
+// Loops > Recursion (usually)
+// Factorial is the famous example
+// package main
+
+// import "fmt"
+
+// func main() {
+// 	fmt.Println(4 * 3 * 2 * 1) // Basic factorial (24)
+// 	n := factorial(4)          // 24
+// 	fmt.Println(n)
+// 	n2 := factorialLoop(4) // loop instead
+// 	fmt.Println(n2)        // 24
+// }
+
+// func factorial(n int) int {
+// 	if n == 0 {
+// 		return 1 // 4 * 3 * 2 * 1 * 1 <-- line 398
+// 	}
+// 	return n * factorial(n-1)
+// }
+
+// func factorialLoop(n int) int {
+// 	total := 1
+// 	for ; n > 0; n-- {
+// 		total *= n
+// 	}
+// 	return total
+// }
