937be3e12ad4e0b578997ba961e4b8e031d9f7a8
George Tipton <gtip22@gmail.com>
2023-02-01 15:16:44 -0700

    Concurrency method sets

---
+++ 20 concurrency/method-sets-revisited.go
@@ -0,0 +1,30 @@
+// method site determines the INTERFACES that the type implements
+
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type circle struct {
+	radius float64
+}
+
+type shape interface {
+	area() float64
+}
+
+func (c *circle) area() float64 {
+	return math.Pi * c.radius * c.radius
+}
+
+func info(s shape) {
+	fmt.Println("area", s.area())
+}
+
+func main() {
+	c := circle{5}
+	// info(c) Doesn't run
+	fmt.Println(c.area())
+}
