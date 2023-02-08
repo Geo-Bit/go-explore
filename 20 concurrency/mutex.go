e461293946378a7833a77cd68a9435d0da616c38
George Tipton <gtip22@gmail.com>
2023-02-08 14:20:05 -0700

    Mutex

--- 20 concurrency/mutex.go
+++ 20 concurrency/mutex.go
@@ -1,6 +1,3 @@
-// Race Conditions
-// Using 
-
 package main
 
 import (
@@ -19,13 +16,17 @@ func main() {
 	var wg sync.WaitGroup
 	wg.Add(gs)
 
+	var mu sync.Mutex
+
 	for i := 0; i < gs; i++ {
 		go func() {
+			mu.Lock()
 			v := counter
 			// time.Sleep(time.Second)
 			runtime.Gosched()
 			v++
 			counter = v
+			mu.Unlock()
 			wg.Done()
 		}()
 		fmt.Println("Goroutines:", runtime.NumGoroutine())
